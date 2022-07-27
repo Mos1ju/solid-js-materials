1603d2a179b9e621d98446af923c1ca9677491cf
Michał <mosiju03@gmail.com>
2022-07-27 21:44:57 +0200

    Add production build webpack config

---
+++ webpack.prod.js
@@ -0,0 +1,44 @@
+const path = require("path");
+
+module.exports = {
+    entry: {
+        components: "./src/components/exports.ts"
+    },
+    mode: "production",
+    resolve: {
+        extensions: [".js", ".jsx", ".ts", ".tsx"]
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(ts)x?$/,
+                exclude: /(node_modules)/,
+                use: {
+                    loader: "babel-loader",
+                }
+            }
+        ]
+    },
+    output: {
+        path: path.resolve(__dirname, "build/release/"),
+        filename: "[name].js",
+        library: {
+            name: "solid-js-materials",
+            type: "umd"
+        }
+    },
+    externals: {
+        "solid-js": {
+            commonjs: "solid-js",
+            commonjs2: "solid-js",
+            amd: "solid-js",
+            root: "solid-js"
+        },
+        "solid-js/web": {
+            commonjs: "solid-js/web",
+            commonjs2: "solid-js/web",
+            amd: "solid-js/web",
+            root: "solid-js/web" 
+        }
+    }
+}
